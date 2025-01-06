c2b49b563434687beb461b6745513c0dfb9e2cba
lambdarw <115614923+lambdarw@users.noreply.github.com>
2025-01-06 15:43:55 +0800

    Update step1_summary.py

--- dataset/step1_summary.py
+++ dataset/step1_summary.py
@@ -5,22 +5,18 @@ from utils import get_llm_prompts, get_llm_output
 import json
 import re
 
-'''
-    Links_to_datasets: https://www.dropbox.com/sh/fu4i5lghdq18cfs/AABZvrPRXs2qal9rlpnFicBDa?dl=0
-'''
 
-# 生成摘要
+# Generate story synopsis
 def generate_summary(title, article, task_type='summary'):
     inputs = {"title": title, "article": article}
     content = get_llm_prompts(task_type, inputs)
     g_summary = get_llm_output(content)
-    # g_summary = eval(g_summary)["abstract"]
     return g_summary
 
-# 数据采样
+# Data sampling
 def df_sample(df, random_percentage=0.02):
     n_rows = len(df)
-    n_sample = int(n_rows * random_percentage)  # 前1%的行数
+    n_sample = int(n_rows * random_percentage)
     n_sample = 2
 
     df_sample = df.head(n_sample)
@@ -28,16 +24,15 @@ def df_sample(df, random_percentage=0.02):
 
 
 if __name__ == "__main__":
-    sample_flag = True
+    sample_flag = False
     summary_flag = True
-    DATASET_NAME = 'WCEP19'  # News14  WCEP18  WCEP19
+    DATASET_NAME = 'News14'  # News14  WCEP19
     article_df = pd.read_json(DATASET_NAME+"/"+DATASET_NAME + "_raw.json")
 
-    '''0.抽取一部分数据测试'''
     if sample_flag:
         article_df = df_sample(article_df)
 
-    '''1.数据处理'''
+    '''1.Data processing'''
     nlp = spacy.load("en_core_web_lg")
     article_df.dropna(subset=['text', 'title'], inplace=True)
     # set corresponding column names. Drop 'story' or 'query' (used to collect stories) column if not available
@@ -45,7 +40,7 @@ if __name__ == "__main__":
     article_df['sentences'] = [[t] for t in article_df.title]
     article_df['sentence_counts'] = ""
 
-    '''2.摘要抽取'''
+    '''2.Story Synopsis Extraction'''
     all_summary = []
     all_sentences_len = []
     j = 0
@@ -65,19 +60,19 @@ if __name__ == "__main__":
         all_sentences_len.append(len(sentences))
 
         j += 1
-        # 暂存文件
+        # Saving files
         with open(DATASET_NAME + "/" + DATASET_NAME + "_all_summary.json", "w") as f:
             json.dump(all_summary, f)
         with open(DATASET_NAME + "/" + DATASET_NAME + "_all_sentences_len.json", "w") as f:
             json.dump(all_sentences_len, f)
 
-    # 写文件
+    # Writing files
     article_df['summary'] = all_summary
     article_df['sentence_counts'] = all_sentences_len
 
-    article_df['date'] = [str(k)[:10] for k in article_df['date']]  # 格式化并排序date列
+    article_df['date'] = [str(k)[:10] for k in article_df['date']]  # Format and sort the date column
     article_df.sort_values(by=['date'], inplace=True)
-    article_df.reset_index(inplace=True, drop=True)  # 重置索引并添加id列
+    article_df.reset_index(inplace=True, drop=True)  # Reset the index and add the id column
     article_df['id'] = article_df.index
     article_df['story'] = [int(story_id) for story_id in article_df["story"]]
 
