005c865fdbae227607c904914f70a4a41a1ee7c6
lambdarw <115614923+lambdarw@users.noreply.github.com>
2025-09-29 16:53:48 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -15,29 +15,6 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import google.generativeai as genai
 from sentence_transformers import SentenceTransformer
-# from google.generativeai.types.generation_types import GenerationConfigType
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_DANGEROUS",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
-    },
-]
 
 args = Args.args
 
@@ -95,10 +72,10 @@ def get_llm_prompts(task_type, inputs):
     return content
 
 def get_llm_output(content, pipe, model_type=args.LLM_mode):
-    with open("/data/zhangruwen/Story2/config/config.yaml", 'r', encoding='utf-8') as file:
+    with open("./config/config.yaml", 'r', encoding='utf-8') as file:
         config = yaml.load(file, yaml.FullLoader)
 
-    if model_type == 'gpt-4o-mini':  # gpt-3.5-turbo-ca
+    if model_type == 'gpt-4o-mini':
         gpt_model = OpenAI(base_url=config["openai"]["base_url"], api_key=config["openai"]["api_key"])
         message = [{"role": "user", "content": content}]
         response = gpt_model.chat.completions.create(
@@ -133,33 +110,22 @@ def get_llm_output(content, pipe, model_type=args.LLM_mode):
 
 # 判断是否是新的类
 def judge_class(max_sim, max_sim_index, re_summary_dict, article, tuned_centers, slide_index, pipe):
-    '''方法1：相似度判断'''
-    # if max_sim > args.thred:
-    #     return max_sim_index, dict()
-    # else:
-    #     return -1, dict()
-
-    '''方法3：LLM间接判断'''
-    if max_sim < 0:  # 新类(max_sim == -1)
+    if max_sim < 0:  # 新类
         re_summary_dict[len(tuned_centers)] = article
         return -1, re_summary_dict
 
     summary_list = re_summary_dict[max_sim_index]  # 取出topk的类标签的summary
-    if max_sim > args.thred:  # 旧的类
+    if max_sim > args.thred:  # 旧类
         inputs_re = {"summary": summary_list, "article": article}
         prompt_re = get_llm_prompts('refresh_summary', inputs_re)
         output_re = get_llm_output(prompt_re, pipe)
         re_summary_dict[max_sim_index] = output_re
         return max_sim_index, re_summary_dict
-    else:  # 旧的类
+    else:  # 旧类
         # (3)问询LLM判断==>获得新类/旧类
         inputs_judg = {"summary": summary_list, "article": article}
         prompt_judg = get_llm_prompts('judgement', inputs_judg)
         output_judg = get_llm_output(prompt_judg, pipe).lower()
-        # with open(args.llm_judgement_output, "a") as file:
-        #     file.write("\n" + str(slide_index))
-        #     file.write("\n" + prompt_judg)
-        #     file.write("\n" + output_judg)
         # (4)更新summary, 返回新类label和更新的summary
         if output_judg[0:3] == 'yes':  # 不是新类, 更新re_summary_dict
             inputs_re = {"summary": summary_list, "article": article}
@@ -167,36 +133,9 @@ def judge_class(max_sim, max_sim_index, re_summary_dict, article, tuned_centers,
             output_re = get_llm_output(prompt_re, pipe)
             re_summary_dict[max_sim_index] = output_re
             return max_sim_index, re_summary_dict
-        else:  # 是新的类, 增加一个新的项re_summary_dict
+        else:  # 是新类, 增加一个新的项summary_dict
             re_summary_dict[len(tuned_centers)] = article
             return -1, re_summary_dict
-
-
-    '''方法2：LLM直接判断（在6/346的时候发生下降）'''
-    # # (1)获取相似度前topk的类标签
-    # # top_class_keys = sorted(sim_dict, key=sim_dict.get, reverse=True)[:args.top_k]
-    # # (2)取出topk的类标签的summary
-    # # summary_list = [re_summary_dict[i] for i in top_class_keys]
-    # summary_list = re_summary_dict[max_sim_index]
-    # # (3)问询判断是否相似==>获得新类/旧类
-    # inputs_judg = {"summary": summary_list, "article": article}
-    # prompt_judg = get_llm_prompts('judgement', inputs_judg)
-    # print(prompt_judg)
-    # output_judg = get_llm_output(prompt_judg).lower()
-    # print(output_judg)
-    # # (4)更新summary, 返回新类label和更新的summary
-    # if output_judg[0:3] == 'yes':  # 不是新类, 更新re_summary_dict
-    #     inputs_re = {"summary": summary_list, "article": article}
-    #     prompt_re = get_llm_prompts('refresh_summary', inputs_re)
-    #     output_re = get_llm_output(prompt_re)
-    #     re_summary_dict[max_sim_index] = output_re
-    #     return max_sim_index, re_summary_dict
-    # else:  # 是新的类, 增加一个新的项re_summary_dict
-    #     # inputs_re = {"article": article}
-    #     # prompt_re = get_llm_prompts('add_summary', inputs_re)
-    #     # output_re = get_llm_output(prompt_re)
-    #     re_summary_dict[len(tuned_centers)] = article
-    #     return -1, re_summary_dict
 
 
 # 使用KBERT提取关键词（获得当前时间窗口的关键字）
@@ -204,7 +143,7 @@ kw_model = KeyBERT(model='/data/zhangruwen/premodel/all-roberta-large-v1')
 def extract_keywords_kbert(docs):
     kw_res = set()
     for doc in docs:
-        # 为了使结果多样化，使用最大边界相关算法(MMR)
+        # 为使结果多样化，使用最大边界相关算法(MMR)
         doc = " ".join(doc)
         keywords = kw_model.extract_keywords(doc, keyphrase_ngram_range=(3, 3), stop_words='english',
                                              use_mmr=True, diversity=0.7)
@@ -247,7 +186,7 @@ def extract_keywords_tfidf(documents, top_n=20):
     return result
 
 # 嵌入
-st_model = SentenceTransformer('/data/zhangruwen/premodel/all-roberta-large-v1').cuda()
+st_model = SentenceTransformer('all-roberta-large-v1').cuda()
 def embedding(texts):
     embedding = st_model.encode(texts)
     return embedding
@@ -267,19 +206,6 @@ def infonce_loss(sample_outputs, class_indices, class_embds, temp=0.2):
     return loss
 
 
-# def infonce_loss(sample_outputs, class_indices, class_embds, temp=0.1):
-#     # 计算所有样本与类的相似度
-#     sims = torch.nn.functional.cosine_similarity(sample_outputs.unsqueeze(1), class_embds.unsqueeze(0), dim=-1)
-#     # 除以温度参数
-#     exp_temp_sims = torch.exp(sims / temp)
-#
-#     # 计算每个样本的正确类的相似度
-#     correct_sims = exp_temp_sims[torch.arange(len(class_indices)), class_indices]
-#
-#     # 计算 InfoNCE 损失
-#     loss = -torch.log(correct_sims / exp_temp_sims.sum(dim=1))
-#     return loss.mean()
-
 
 # 评估指标
 def eval_metric(label, cluster):
@@ -293,7 +219,6 @@ def eval_metric(label, cluster):
 
 
 def adjust_learning_rate(optimizer, epoch, num_epochs, start_lr=0.001, min_lr=0.00001, warmup_epochs=20):
-    """Decay the learning rate based on the schedule"""
     if epoch < warmup_epochs:
         lr = start_lr * (epoch + 1) / warmup_epochs
     else:
@@ -310,6 +235,7 @@ def adjust_learning_rate(optimizer, epoch, num_epochs, start_lr=0.001, min_lr=0.
         param_group["lr"] = lr
 
     return lr
+
 
 
 
